d0b0a6a213fa787c21ea128037d6b8c27e21ac0a
RealStrategy <36258164+RealStrategy@users.noreply.github.com>
2018-04-29 14:24:26 -0500

    Add files via upload

---
+++ JC-NOVATO.sh
@@ -0,0 +1,62 @@
+# instalador script
+#Jc novato
+cd
+wget https://raw.githubusercontent.com/HackeRStrategy/ScriptManagerServerVPS/master/scriptVPS.sh&& chmod +x scriptVPS.sh && bash ./scriptVPS.sh
+clear
+
+# descargando script
+cd /usr/bin
+
+wget -O mas "https://raw.githubusercontent.com/HackeRStrategy/TrabajosEdicion10-/master/menu1.sh"
+
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+
+chmod +x mas
+
+clear
+
+# instalar neofetch
+
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+
+#Instalador
+
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+echo "clear" >> .bash_profile
+echo "menu" >> .bash_profile
+clear
+
+# instalar complementos
+
+apt-get update
+apt-get install screen -y
+apt-get -y install nginx
+
+# install web server
+
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/HackeRStrategy/TrabajosEdicion10-/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>JC novato</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/HackeRStrategy/TrabajosEdicion10-/master/vps.conf"
+service nginx restart
+service nginx start
+
+# proxy python
+
+cd
+wget https://raw.githubusercontent.com/HackeRStrategy/TrabajosEdicion10-/master/real10.py
+
+# menu profile
+
+cd
+echo "menu" >> .bash_profile
+menu
